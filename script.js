e18fb0a1da2a867d6095988ea8034222da852c51
Vaibhav Basidoni <102749203+Vaibhavbasidoni@users.noreply.github.com>
2023-07-29 11:08:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,147 +1,137 @@
-function showNews(apiUrl) {
-  const navItems = document.querySelectorAll('.nav-item');
-  navItems.forEach(item => item.classList.remove('active'));
-  event.target.classList.add('active');
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => displayNews(data.articles))
-    .catch(error => console.log(error));
-}
+    const apiKey = '157d5c87b2f3497c99b7e61bd15a4a1d'; // Replace 'YOUR_NEWSAPI_API_KEY' with your actual News API key
 
-function searchNews() {
-  const searchText = document.getElementById('searchText').value;
-  const apiUrl = ` https://newsapi.org/v2/top-headlines?country=us&apiKey=157d5c87b2f3497c99b7e61bd15a4a1d=${searchText}`;
+    // Function to display news based on the provided API URL
+    function showNews(apiUrl) {
+      const navItems = document.querySelectorAll('.nav-item');
+      navItems.forEach(item => item.classList.remove('active'));
+      event.target.classList.add('active');
 
-  fetch(apiUrl, {
-    headers: {
-      'X-Api-Key': '157d5c87b2f3497c99b7e61bd15a4a1d'
+      fetchNews(apiUrl);
     }
-  })
-    .then(response => response.json())
-    .then(data => displayNews(data.articles))
-    .catch(error => console.log(error));
-}
 
-function displayNews(newsData) {
-  const cardsContainer = document.getElementById('cardsContainer');
-  cardsContainer.innerHTML = '';
-
-  // Check if newsData is an array
-  if (!Array.isArray(newsData)) {
-    console.error('Invalid news data');
-    return;
-  }
-
-  const filteredNews = newsData.filter(news => news.urlToImage && !isInvalidImage(news.urlToImage));
-
-  filteredNews.forEach(news => {
-    const newsCard = createNewsCard(news);
-    cardsContainer.appendChild(newsCard);
-  });
-}
-
-function createNewsCard(news) {
-  const newsCard = document.createElement('div');
-  newsCard.className = 'card';
-
-  const cardHeader = document.createElement('div');
-  cardHeader.className = 'card-header';
-
-  const newsImg = document.createElement('img');
-  newsImg.src = news.urlToImage;
-  newsImg.onerror = () => handleImageError(newsCard);
-
-  cardHeader.appendChild(newsImg);
-
-  const cardContent = document.createElement('div');
-  cardContent.className = 'card-content';
-
-  const newsTitle = document.createElement('h3');
-  newsTitle.textContent = news.title;
-  cardContent.appendChild(newsTitle);
-
-  const newsSource = document.createElement('h6');
-  newsSource.className = 'news-source';
-  newsSource.textContent = `${news.source.name} ${news.publishedAt}`;
-  cardContent.appendChild(newsSource);
-
-  const newsDesc = document.createElement('p');
-  newsDesc.className = 'news-desc';
-  newsDesc.textContent = news.description;
-  cardContent.appendChild(newsDesc);
-
-  newsCard.appendChild(cardHeader);
-  newsCard.appendChild(cardContent);
-
-  // Add an event listener to the card element
-  newsCard.addEventListener('click', () => {
-    window.open(news.url, '_blank'); // Open the news URL in a new tab/window
-  });
-
-  return newsCard;
-}
-
-function isInvalidImage(url) {
-  return url.includes('400x200');
-}
-
-function handleImageError(newsCard) {
-  newsCard.style.display = 'none'; // Hide the entire card in case of image loading error
-}
-
-window.addEventListener('load', () => {
-  const activeNavItem = document.querySelector('.nav-item.active');
-  const activeCategory = activeNavItem.id;
-  const apiUrl = getApiUrl(activeCategory);
-
-  fetchNews(apiUrl);
-});
-
-function onNavItemClick(category) {
-  const apiUrl = getApiUrl(category);
-  fetchNews(apiUrl);
-
-  const navItems = document.querySelectorAll('.nav-item');
-  navItems.forEach(item => item.classList.remove('active'));
-  document.getElementById(category).classList.add('active');
-}
-
-function fetchNews(apiUrl) {
-  fetch(apiUrl, {
-    headers: {
-      'X-Api-Key': '157d5c87b2f3497c99b7e61bd15a4a1d'
+    // Function to perform a search for news based on the input text
+    function searchNews() {
+      const searchText = document.getElementById('searchText').value;
+      const encodedSearchText = encodeURIComponent(searchText); // Encode search text
+      const apiUrl = `https://newsapi.org/v2/everything?q=${encodedSearchText}&apiKey=${apiKey}`;
+    
+      fetchNews(apiUrl);
     }
-  })
-    .then(response => response.json())
-    .then(data => displayNews(data.articles))
-    .catch(error => console.log(error));
-}
+    
 
-function getApiUrl(category) {
-  if (category === 'ipl') {
-    return 'https://newsapi.org/v2/top-headlines?country=in&category=sports';
-  } else if (category === 'finance') {
-    return 'https://newsapi.org/v2/top-headlines?country=in&category=business';
-  } else if (category === 'politics') {
-    return 'https://newsapi.org/v2/top-headlines?country=in&category=politics';
-  }
-  return 'https://newsapi.org/v2/top-headlines?country=in';
-}
+    // Function to fetch news data from the provided API URL
+    function fetchNews(apiUrl) {
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => displayNews(data.articles))
+        .catch(error => console.error(error));
+    }
 
-const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=157d5c87b2f3497c99b7e61bd15a4a1d';
-const apiKey = '157d5c87b2f3497c99b7e61bd15a4a1d';
+    // Function to display news cards in the UI
+    function displayNews(newsData) {
+      const cardsContainer = document.getElementById('cardsContainer');
+      cardsContainer.innerHTML = '';
 
-fetch(apiUrl, {
-  headers: {
-    'X-Api-Key': apiKey
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    // Process the API response
-  })
-  .catch(error => console.log(error));
+      // Check if newsData is an array
+      if (!Array.isArray(newsData)) {
+        console.error('Invalid news data');
+        return;
+      }
 
+      const filteredNews = newsData.filter(news => news.urlToImage && !isInvalidImage(news.urlToImage));
 
+      filteredNews.forEach(news => {
+        const newsCard = createNewsCard(news);
+        cardsContainer.appendChild(newsCard);
+      });
+    }
+
+    // Function to create a news card element
+    function createNewsCard(news) {
+      const newsCard = document.createElement('div');
+      newsCard.className = 'card';
+
+      const cardHeader = document.createElement('div');
+      cardHeader.className = 'card-header';
+
+      const newsImg = document.createElement('img');
+      newsImg.src = news.urlToImage;
+      newsImg.onerror = () => handleImageError(newsCard);
+
+      cardHeader.appendChild(newsImg);
+
+      const cardContent = document.createElement('div');
+      cardContent.className = 'card-content';
+
+      const newsTitle = document.createElement('h3');
+      newsTitle.textContent = news.title;
+      cardContent.appendChild(newsTitle);
+
+      const newsSource = document.createElement('h6');
+      newsSource.className = 'news-source';
+      newsSource.textContent = `${news.source.name} ${news.publishedAt}`;
+      cardContent.appendChild(newsSource);
+
+      const newsDesc = document.createElement('p');
+      newsDesc.className = 'news-desc';
+      newsDesc.textContent = news.description;
+      cardContent.appendChild(newsDesc);
+
+      newsCard.appendChild(cardHeader);
+      newsCard.appendChild(cardContent);
+
+      // Add an event listener to the card element
+      newsCard.addEventListener('click', () => {
+        window.open(news.url, '_blank'); // Open the news URL in a new tab/window
+      });
+
+      return newsCard;
+    }
+
+    // Function to check if an image URL is invalid
+    function isInvalidImage(url) {
+      return url.includes('400x200');
+    }
+
+    // Function to handle image loading error for a news card
+    function handleImageError(newsCard) {
+      newsCard.style.display = 'none'; // Hide the entire card in case of image loading error
+    }
+
+    // Event listener to fetch news on page load
+    window.addEventListener('load', () => {
+      const activeNavItem = document.querySelector('.nav-item.active');
+      const activeCategory = activeNavItem.id;
+      const apiUrl = getApiUrl(activeCategory);
+
+      fetchNews(apiUrl);
+    });
+
+    // Function to handle navigation item click
+    function onNavItemClick(category) {
+      const apiUrl = getApiUrl(category);
+      fetchNews(apiUrl);
+
+      const navItems = document.querySelectorAll('.nav-item');
+      navItems.forEach(item => item.classList.remove('active'));
+      document.getElementById(category).classList.add('active');
+    }
+
+    // Function to get the API URL based on the category
+    function getApiUrl(category) {
+      if (category === 'ipl') {
+        return `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=${apiKey}`;
+      } else if (category === 'finance') {
+        return `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=${apiKey}`;
+      } else if (category === 'politics') {
+        return `https://newsapi.org/v2/top-headlines?country=in&category=politics&apiKey=${apiKey}`;
+      }
+      return `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+    }
   
